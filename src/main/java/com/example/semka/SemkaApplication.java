51eb7d6207b30795ec8fceee274b5151d1ef1a94
NikitaOomory <hackyourw@gmail.com>
2023-02-05 16:58:31 +0300

    Первая версия

---
+++ src/main/java/com/example/semka/SemkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.semka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/semka/SemkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
